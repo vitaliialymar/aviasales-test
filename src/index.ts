3a99fe58178465bf00ba57645ceb4e315d1d9f41
Vitaliia Lymar <vitaliialymar@gmail.com>
2023-06-01 17:38:38 +0300

    add class metod

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import './style.css';
-import 'vanillajs-datepicker/css/datepicker.css';
-import { Datepicker } from 'vanillajs-datepicker';
-import ru from 'vanillajs-datepicker/locales/ru';
-import i18next from 'i18next';
-import resources from './locales/index';
-import createWidgetForm from './formWidget';
+import './style.css'
+import 'vanillajs-datepicker/css/datepicker.css'
+import { Datepicker } from 'vanillajs-datepicker'
+import ru from 'vanillajs-datepicker/locales/ru'
+import i18next from 'i18next'
+import resources from './locales/index'
+import FormWidget from './formWidget'
 
-Object.assign(Datepicker.locales, ru);
+Object.assign(Datepicker.locales, ru)
 
 const app = () => {
   i18next.init({
@@ -17,12 +17,18 @@ const app = () => {
     const lng = document.getElementById('lng')
     lng.textContent = i18next.t('lng')
     lng.addEventListener('click', () => {
-      i18next.language === 'en' ? i18next.changeLanguage("ru").then(() => createWidgetForm('form-widget', '#F5A623', '#4A90E2', '#FFFFFF', i18next)) :
-      i18next.changeLanguage("en").then(() => createWidgetForm('form-widget', '#F5A623', '#4A90E2', '#FFFFFF', i18next))
+        if (i18next.language === 'en') {
+          i18next.changeLanguage("ru").then(() => widget.render())
+          lng.textContent = i18next.t('lng')
+        } else {
+          i18next.changeLanguage("en").then(() => widget.render())
+          lng.textContent = i18next.t('lng')
+        }
       })
   })
 
-  createWidgetForm('form-widget', '#F5A623', '#4A90E2', '#FFFFFF', i18next)
+  const widget = new FormWidget('form-widget', '#F5A623', '#4A90E2', '#FFFFFF', i18next)
+  widget.render()
 }
 
 app()
